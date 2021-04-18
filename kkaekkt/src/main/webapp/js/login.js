eca3d3d2bbd3d2033ffeb55daa443a276f3d7083
DESKTOP-VB5L3JM\cheun <cheung_co@naver.com>
2021-04-18 16:16:13 +0900

    프로젝트 업로드

--- kkaekkt/src/main/webapp/js/login.js
+++ kkaekkt/src/main/webapp/js/login.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
 		
 		$("#modal_show").click(function() {
-			$('#mask').show()
 			$("#modal_container").show()
 		})
 		$("#modal_close").click(function() {
 			$("#modal_container").hide()
-			$('#mask').hide()
 		})
 	})
 
@@ -14,10 +12,9 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $('#login').click(function(){ 
-    login()
-  })
-})
-
+    login();
+  });
+});
 function login() {
   var userData={
     id:$('#id').val(),
@@ -27,9 +24,8 @@ function login() {
     url:"/login.do",
     data:userData,
     success:function(result) {
-    console.log("로그인js" + result)
       if(result=='fail'){
-        alert('id 혹은 password가 일치하지 않습니다.');
+        alert('아이디 또는 비밀번호가 일치하지 않습니다.');
         
       }else if(result == 1){
         location.href="/index.do";
